90b52cf6b3fd05dac8c4b8389f002cdeccbdab48
Kito Clark <kitoclark1989@gmail.com>
2018-03-30 16:07:38 +1000

    Added show, edit and destroy functionality to site

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,31 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def create
 
     @post = Post.create(post_params)
 
     redirect_to posts_path
+  end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path(@post)
   end
 
   private
